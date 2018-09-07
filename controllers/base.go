89abfda9d5eccc10c7c7d61252b1dc6127424cf1
Vassiliy Kryazhev <vassiliy.kryazhev@gmail.com>
2018-09-07 19:01:07 +0300

    Dependencies were changed.

--- controllers/base.go
+++ controllers/base.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"github.com/astaxie/beego"
 	"github.com/kryazhev/trucking/models"
-	"net/http"
 	"strings"
 )
 
@@ -37,31 +36,11 @@ func (c *AppController) ajaxResponseFail(message string) {
 }
 
 func (c *AppController) Prepare() {
-	/*	if c.Ctx.Request.TLS == nil {
-			url := redirectURL(c.Ctx.Request)
-			c.Redirect(url, 301)
-		}
-	*/if c.Data["Lang"] == nil {
+	if c.Data["Lang"] == nil {
 		lang := c.Ctx.GetCookie("lang")
 		if len(lang) == 0 {
 			lang = "ru"
 		}
 		c.Data["Lang"] = lang
-	}
-}
-
-func redirectURL(r *http.Request) string {
-	if r.URL.IsAbs() {
-		return strings.Replace(r.URL.RawQuery, "http", "https", 1)
-	} else {
-		return "https://" + host(r) + "/home.html"
-	}
-}
-
-func host(r *http.Request) string {
-	if len(r.Host) > 0 {
-		return r.Host
-	} else {
-		return "localhost"
 	}
 }
